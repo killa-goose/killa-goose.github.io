5c3210556eada8ad97f87506accd27c7813c2ad3
Angus.Lee <Angus.Lee@vocalink.com>
2021-03-19 14:48:50 +0000

    Remove AJAX request - already moved to index.js

--- js/models.js
+++ js/models.js
@@ -34,22 +34,3 @@ var ChampionshipView = Backbone.View.extend({
     });
   },
 });
-var chmpsData = $.ajax({
-    url: "resources/data/championships.json",
-    success: function(data){
-      console.log('AJAX success!');
-      console.log(data);
-      $('.container section:nth-child(3)').text(JSON.stringify(data));
-      console.log('Constructing ChampionshipsCollection...')
-      for (i=0; i < data.championships.length; i++) {
-        let c = data.championships[i];
-        console.log(c);
-        let m = new Championship(c);
-        Championships.add(m);
-      }
-    console.log(Championships);
-    },
-    error: function(xhr, status, error) {
-      console.log(status, error);
-    }
-});
